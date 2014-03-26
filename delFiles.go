631bd9ba650e074b070b0219f381009388a1d9ef
codebook <code.fun.dev@gmail.com>
2014-03-26 14:59:18 +0800

    modify tab to space

--- delFiles.go
+++ delFiles.go
@@ -17,12 +17,12 @@ func DelFiles(dir,reg string) {
   for _,file := range files {
     fileName := file.Name()
     if isDir := file.IsDir(); isDir {
-	  DelFiles(dir+"/"+fileName,reg)
+      DelFiles(dir+"/"+fileName,reg)
 	} else {
-	  if matched,_ := regexp.MatchString(reg,fileName); !matched {
-	    os.Remove(dir+"/"+fileName)
-	  }
-	}
+      if matched,_ := regexp.MatchString(reg,fileName); !matched {
+        os.Remove(dir+"/"+fileName)
+      }
+    }
   }
   // 如果文件夹中的文件都已经被删除，则将该空文件夹删除
   files,err = ioutil.ReadDir(dir)
@@ -36,15 +36,15 @@ func main() {
   
   if len(os.Args) != 3 {
     fmt.Println("Usage: command basedir reg-name\nbasedir:where to find the files\nreg-name:the string to mathch the filename")
-	return
+    return
   } else {
     _,err := os.Open(os.Args[1])
-	if err != nil {
-	  if exist := os.IsExist(err); !exist {
-	    fmt.Println(os.Args[1]+" is not dir")
-	    return
-	  }
-	}
+    if err != nil {
+      if exist := os.IsExist(err); !exist {
+        fmt.Println(os.Args[1]+" is not dir")
+        return
+      }
+    }
   }
   baseDir := os.Args[1]
   regName := os.Args[2]+".*"
